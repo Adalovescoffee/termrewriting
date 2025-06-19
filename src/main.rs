41df0b6385d9322250e7ffbee46e8b18cc4a3162
adalovescoffee <anassboufrioua@gmail.com>
2025-06-19 06:18:44 +0200

    core of parser + generated tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,42 @@
 mod parser;
+mod lexer;
+use lexer::Lexer;
+use parser::{Parser, Node, ParserError};
 fn main() {
-    println!("Hello, world!");
+    // Test expressions
+    let expressions = vec![
+        "1 + 2 * 3",
+        "5 - x / y",
+        "(2 + 3) * 4",
+        "a b + c d", // Implicit multiplication: a*b + c*d
+        "10 * (x + 5)",
+        "x + y * z - 1",
+        "2(x + 3)", // Implicit multiplication: 2*(x+3)
+        "a = b + 1", // Example with assignment (if you implement parsing statements later)
+        "x == y",    // Example with equality (if you implement parsing comparisons later)
+        "123",       // Single number
+        "z",         // Single variable
+        "5 +",       // Error case: incomplete expression
+        "3 * (4 + 5",// Error case: missing parenthesis
+        "# invalid", // Error case: illegal character
+    ];
+
+    for (i, expr_str) in expressions.into_iter().enumerate() {
+        println!("\n--- Parsing Expression {} ---", i + 1);
+        println!("Input: \"{}\"", expr_str);
+
+        let input = expr_str.to_string();
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        match parser.parse_term() {
+            Ok(ast) => {
+                println!("Successfully Parsed AST: {:?}", ast);
+            },
+            Err(e) => {
+                eprintln!("Parsing Error: {:?}", e);
+            }
+        }
+    }
 }
+
--- src/parser.rs
+++ src/parser.rs
@@ -1,113 +1,171 @@
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum Operator {
-    Add,
-    Substract, 
-    Multiply,
-    Divide, 
+use std::any::Any;
 
-}
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum Term {
-    Number(u64),
-    Operation(Box<Term>, Operator, Box<Term>),
-
-
-}
-pub enum TokenType{
-    Plus, 
-    Minus, 
-    Multiply, 
-    Divide, 
-    LParen, 
-    RParen,
-    Integer(i64),
+use crate::lexer::{Lexer,TokenType};
+#[derive(Debug,PartialEq)]
+pub enum Node{
+    Number(i64),
     Variable(char),
-    Eof,
-    Illegal,
-    Equal,
+    BinaryOp(Box<Node>,Operator,Box<Node>)
+
 }
-pub struct Lexer{
-    input: String,
-    position: usize,
-    read_position: usize,
-    ch: char,
+#[derive(Debug, PartialEq)]
+pub enum ParserError {
+    UnexpectedToken { expected: TokenType, found: TokenType, position: usize },
+    ExpectedNumber { found: TokenType, position: usize },
+    ExpectedVariable { found: TokenType, position: usize },
+    SyntaxError(String),
+    LexerError(String), // Placeholder if lexer errors need to be propagated
 }
-impl Lexer {
-    fn new(expression: String) ->Lexer{
-       let mut lexer =  Lexer{
-            input : expression,
-            position : 0,
-            read_position : 0, 
-            ch: '\0',
-        
-        };
-        lexer.read_char();
-        lexer 
-    }
-    // reads character idk why i'm writing these commnets atp the code speaks for itself
-    fn read_char(&mut self){
-        if self.read_position>= self.input.len(){
-            self.ch = '\0';
-            self.position = self.read_position;
+#[derive(Debug,PartialEq)]
+pub enum Operator{
+    Add,
+    Substract,
+    Multiply,
+    Divide,
+
+}
+pub struct Parser {
+    lexer: Lexer,
+    current_token: TokenType,
+    peek_token: TokenType,
+}
+impl Parser {
+    pub fn new(mut lexer: Lexer)-> Parser{
+        let first_token = lexer.get_token();
+        let second_token = lexer.get_token();
+        Parser{
+            lexer:lexer,
+            current_token:first_token,
+            peek_token:second_token,
 
         }
-        else{
-
-            self.ch = self.input.as_bytes()[self.read_position] as char;
-        }
-        self.position = self.read_position; 
-        self.read_position+=1;
     }
-    fn read_number(&mut self)->String{
-        let start_position = self.position;
-        
-        while self.ch.is_numeric()== true {
-            self.read_char();
+    fn advance(&mut self){
+        self.current_token = self.peek_token;
+        self.peek_token = self.lexer.get_token();
+
+
+    }
+ 
+    fn current_token_is(&self, token_type: TokenType) -> bool {
+        self.current_token == token_type
+    }
+    fn peek_token_is(&self, token_type: TokenType) -> bool {
+        self.peek_token == token_type
+    }
+    fn expect_and_advance(&mut self, expected_type: TokenType) -> Result<(), ParserError> {
+        if self.current_token_is(expected_type) {
+            self.advance();
+            Ok(())
+        } else {
             
-
-        } 
-        self.input[start_position..self.position].to_string()
-        
-
-    }
-    //skips " "
-    fn skip_whitespace(&mut self) {
-        while self.ch.is_whitespace() {
-            self.read_char();
+            Err(ParserError::UnexpectedToken {
+                expected: expected_type,
+                found: self.current_token.clone(),
+                position: 0, // TODO: Get position from lexer
+            })
         }
-    }
-    //gets token 
-    fn get_token(&mut self)->TokenType{
-        self.skip_whitespace();
-        let token = self.ch;
-        let token_type = match self.ch {
-            '+' => TokenType::Plus,
-            '-' => TokenType::Minus,
-            '*' => TokenType::Multiply,
-            '/' => TokenType::Divide,
-            '(' => TokenType::LParen,
-            ')' => TokenType::RParen, 
-            '=' => TokenType::Equal,
-            '\0' =>TokenType::Eof,
-            c @ '0'..='9' => {
-                let num_str = self.read_number();
-                TokenType::Integer(num_str.parse().expect("oops smt went wrong mbmb"))
 
+
+
+
+
+
+
+
+    
+    }
+    
+    fn parse_factor(&mut self)->Result<Node, ParserError>{
+    let node = match self.current_token{
+        TokenType::Integer(value)=>{
+            self.advance();
+            Ok(Node::Number(value  ))
+
+        },
+        TokenType::Variable(value)=>{
+            self.advance();
+            Ok(Node::Variable(value))
+        },
+        TokenType::LParen =>{
+            self.expect_and_advance(TokenType::LParen)?;
+            let node = self.parse_tuah()?;
+            self.expect_and_advance(TokenType::RParen)?;
+            Ok(node)
+        },
+        _ => Err(ParserError::UnexpectedToken {
+            expected: TokenType::Integer(42), // 
+            found: self.current_token.clone(),
+            position: 0, // to be fixed
+        }),
+    };
+    node
+    }
+    // for * / and stuff inside () 
+    fn parse_tuah(&mut self)-> Result<Node, ParserError>{
+        let mut lhs = self.parse_factor()?;
+        while self.peek_token_is(TokenType::Multiply) ||
+      self.peek_token_is(TokenType::Divide)   ||
+      matches!(self.peek_token, TokenType::Integer(_)) ||
+      matches!(self.peek_token, TokenType::Variable(_)) ||
+      self.peek_token_is(TokenType::LParen){
+        if self.peek_token_is(TokenType::Multiply){
+            self.advance();
+            self.advance();
+            let rhs = self.parse_factor()?;
+            lhs = Node::BinaryOp(Box::new(lhs), Operator::Multiply,Box::new(rhs));
+
+        }
+       else if self.peek_token_is(TokenType::Divide){
+            self.advance();
+            self.advance();
+            let rhs = self.parse_factor()?;
+            lhs = Node::BinaryOp(Box::new(lhs), Operator::Divide, Box::new(rhs));
+
+        }
+        else if matches!(self.peek_token, TokenType::Integer(_))|| self.peek_token_is(TokenType::LParen)||matches!(self.peek_token, TokenType::Variable(_)){
+            self.advance();
+            let rhs = self.parse_factor()?;
+            lhs = Node::BinaryOp(Box::new(lhs), Operator::Multiply, Box::new(rhs));
+
+        }
+        
+      }
+      Ok(lhs)
+
+        
+    }
+    pub fn parse_term(&mut self)-> Result<Node, ParserError> {
+        
+        let mut lhs = self.parse_tuah()?; 
+
+        
+        loop {
+            let operator: Operator;
+
+            match self.peek_token {
+                TokenType::Plus => {
+                    operator = Operator::Add;
+                },
+                TokenType::Minus => {
+                    operator = Operator::Substract;
+                },
+                // If it's not '+' or '-', tis over
+                _ => break,
             }
-            c if c.is_alphabetic() => TokenType::Variable(c),
-            _ => TokenType::Illegal,
-        };
-        match token_type {
-            TokenType::Plus | TokenType::Minus | TokenType::Multiply | TokenType::Divide |
-            TokenType::LParen | TokenType::RParen | TokenType::Equal |
-            TokenType::Variable(_) | TokenType::Illegal => self.read_char(),
-            
-            TokenType::Integer(_) | TokenType::Eof => {}
+            match operator {
+                Operator::Add => self.expect_and_advance(TokenType::Plus)?,
+                Operator::Substract => self.expect_and_advance(TokenType::Minus)?,
+                _ => unreachable!("Should only be Add or Subtract here"), 
+            }
+
+           
+            let rhs = self.parse_tuah()?; 
+
+            lhs = Node::BinaryOp(Box::new(lhs), operator, Box::new(rhs));
         }
-        token_type
+
+       
+        Ok(lhs)
     }
 }
-
-
-
-//now i implement parser 
